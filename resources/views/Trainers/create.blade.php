779d5ee2e77645638541b6ae825572e18ce04e63
Alvaro Gabriel Gomez Andazol <alvarogabrielgomez@gmail.com>
2019-03-14 14:44:29 -0300

    init

---
+++ resources/views/Trainers/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Trainers Create')
+
+@section('content')
+<div class="container">
+
+    <form class="form-group" method="POST" action="/trainers">
+        @csrf
+        <div class="form-group">
+            <label for="">Nombre</label>
+            <input type="text" class="form-control" name="name">      
+        </div>
+
+        <button type="submit" class="btn btn-primary">Guardar</button>
+    </form>
+    
+</div>
+@endsection 
