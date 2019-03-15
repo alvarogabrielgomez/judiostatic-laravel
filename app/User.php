f7c60bae6503e015ec36e9836dbd6682366cd7f7
Alvaro Gabriel Gomez Andazol <alvarogabrielgomez@gmail.com>
2019-03-15 16:51:32 -0300

    asdas

--- app/User.php
+++ app/User.php
@@ -10,6 +10,9 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function roles(){
+        return $this->belongstoMany('judiostatic\Role');
+    }
     /**
      * The attributes that are mass assignable.
      *
